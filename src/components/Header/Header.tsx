69b29332e1224be2d38f2f2e82a1c22fdbb2f5d8
v-m-dev <morozvitalii2403@gmail.com>
2024-07-09 17:23:15 +0300

    feat: save search input to local storage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,33 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 
-class Header extends Component {
+interface HeaderProps {}
+
+interface HeaderState {
+  inputValue: string;
+}
+
+class Header extends Component<HeaderProps, HeaderState> {
+  constructor(props: HeaderProps) {
+    super(props);
+    this.saveInputToLocalStorage = this.saveInputToLocalStorage.bind(this);
+    this.state = { inputValue: localStorage.getItem('searchInput') || '' };
+  }
+
+  saveInputToLocalStorage(event: React.ChangeEvent<HTMLInputElement>) {
+    localStorage.setItem('searchInput', event.target.value);
+    this.setState({ inputValue: event.target.value });
+  }
+
   render() {
     return (
       <header className="header">
-        <input className="header-input" type="search" />
+        <input
+          className="header-input"
+          type="search"
+          value={this.state.inputValue}
+          onChange={this.saveInputToLocalStorage}
+        />
         <button className="header-btn" type="button">
           Search
         </button>
